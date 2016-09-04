4dbaa44f30ce60e8d3e0a1ba1f6f91e1ae03958f
RandomTiger <RandomSnowTiger@gmail.com>
2016-09-04 11:26:13 +0100

    Add support for Vive controller

---
+++ Samples/DaydreamLabsControllerPlayground/Assets/SteamVR/Extras/SteamVR_TrackedController.cs
@@ -0,0 +1,301 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public struct ClickedEventArgs
+{
+    public uint controllerIndex;
+    public uint flags;
+    public float padX, padY;
+}
+
+public delegate void ClickedEventHandler(object sender, ClickedEventArgs e);
+
+public class SteamVR_TrackedController : MonoBehaviour
+{
+    public uint controllerIndex;
+    public VRControllerState_t controllerState;
+    public bool triggerPressed = false;
+    public bool steamPressed = false;
+    public bool menuPressed = false;
+    public bool padPressed = false;
+    public bool padTouched = false;
+    public bool gripped = false;
+
+    public event ClickedEventHandler MenuButtonClicked;
+    public event ClickedEventHandler MenuButtonUnclicked;
+    public event ClickedEventHandler TriggerClicked;
+    public event ClickedEventHandler TriggerUnclicked;
+    public event ClickedEventHandler SteamClicked;
+    public event ClickedEventHandler PadClicked;
+    public event ClickedEventHandler PadUnclicked;
+    public event ClickedEventHandler PadTouched;
+    public event ClickedEventHandler PadUntouched;
+    public event ClickedEventHandler Gripped;
+    public event ClickedEventHandler Ungripped;
+
+    public bool padTouchedDown;
+    public bool padTouchedUp;
+
+    public bool MenuButtonDown;
+    public bool MenuButtonUp;
+
+    public bool padPressedUp;
+    public bool padPressedDown;
+
+    public bool grippedUp;
+    public bool grippedDown;
+
+    public bool triggerPressedUp;
+    public bool triggerPressedDown;
+
+    public Vector2 touchPos = Vector2.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (this.GetComponent<SteamVR_TrackedObject>() == null)
+        {
+            gameObject.AddComponent<SteamVR_TrackedObject>();
+        }
+
+		if (controllerIndex != 0)
+		{
+			this.GetComponent<SteamVR_TrackedObject>().index = (SteamVR_TrackedObject.EIndex)controllerIndex;
+			if (this.GetComponent<SteamVR_RenderModel>() != null)
+			{
+				this.GetComponent<SteamVR_RenderModel>().index = (SteamVR_TrackedObject.EIndex)controllerIndex;
+			}
+		}
+		else
+		{
+			controllerIndex = (uint) this.GetComponent<SteamVR_TrackedObject>().index;
+        }
+    }
+
+	public void SetDeviceIndex(int index)
+	{
+			this.controllerIndex = (uint) index;
+	}
+
+	public virtual void OnTriggerClicked(ClickedEventArgs e)
+    {
+        if (TriggerClicked != null)
+            TriggerClicked(this, e);
+    }
+
+    public virtual void OnTriggerUnclicked(ClickedEventArgs e)
+    {
+        if (TriggerUnclicked != null)
+            TriggerUnclicked(this, e);
+    }
+
+    public virtual void OnMenuClicked(ClickedEventArgs e)
+    {
+        if (MenuButtonClicked != null)
+            MenuButtonClicked(this, e);
+    }
+
+    public virtual void OnMenuUnclicked(ClickedEventArgs e)
+    {
+        if (MenuButtonUnclicked != null)
+            MenuButtonUnclicked(this, e);
+    }
+
+    public virtual void OnSteamClicked(ClickedEventArgs e)
+    {
+        if (SteamClicked != null)
+            SteamClicked(this, e);
+    }
+
+    public virtual void OnPadClicked(ClickedEventArgs e)
+    {
+        if (PadClicked != null)
+            PadClicked(this, e);
+    }
+
+    public virtual void OnPadUnclicked(ClickedEventArgs e)
+    {
+        if (PadUnclicked != null)
+            PadUnclicked(this, e);
+    }
+
+    public virtual void OnPadTouched(ClickedEventArgs e)
+    {
+        if (PadTouched != null)
+            PadTouched(this, e);
+    }
+
+    public virtual void OnPadUntouched(ClickedEventArgs e)
+    {
+        if (PadUntouched != null)
+            PadUntouched(this, e);
+    }
+
+    public virtual void OnGripped(ClickedEventArgs e)
+    {
+        if (Gripped != null)
+            Gripped(this, e);
+    }
+
+    public virtual void OnUngripped(ClickedEventArgs e)
+    {
+        if (Ungripped != null)
+            Ungripped(this, e);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		var system = OpenVR.System;
+		if (system != null && system.GetControllerState(controllerIndex, ref controllerState))
+		{
+            touchPos.x = controllerState.rAxis0.x;
+            touchPos.y = controllerState.rAxis0.y;
+
+            ulong trigger = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_SteamVR_Trigger));
+            if (trigger > 0L && !triggerPressed)
+            {
+                triggerPressedDown = true;
+                triggerPressed = true;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnTriggerClicked(e);
+            }
+            else if (trigger == 0L && triggerPressed)
+            {
+                triggerPressedUp = true;
+                triggerPressed = false;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnTriggerUnclicked(e);
+            }
+            else
+            {
+                triggerPressedUp = false;
+                triggerPressedDown = false;
+            }
+
+            ulong grip = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_Grip));
+            if (grip > 0L && !gripped)
+            {
+                grippedDown = true;
+                gripped = true;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnGripped(e);
+            }
+            else if (grip == 0L && gripped)
+            {
+                grippedUp = true;
+                gripped = false;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnUngripped(e);
+            }
+            else
+            {
+                grippedUp = false;
+                grippedDown = false;
+            }
+
+            ulong pad = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_SteamVR_Touchpad));
+            if (pad > 0L && !padPressed)
+            {
+                padPressedDown = true;
+                padPressed = true;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnPadClicked(e);
+            }
+            else if (pad == 0L && padPressed)
+            {
+                padPressedUp = true;
+                padPressed = false;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnPadUnclicked(e);
+            }
+            else
+            {
+                padPressedUp = false;
+                padPressedDown = false;
+            }
+
+            ulong menu = controllerState.ulButtonPressed & (1UL << ((int)EVRButtonId.k_EButton_ApplicationMenu));
+            if (menu > 0L && !menuPressed)
+            {
+                MenuButtonDown = true;
+                menuPressed = true;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnMenuClicked(e);
+            }
+            else if (menu == 0L && menuPressed)
+            {
+                MenuButtonUp = true;
+                menuPressed = false;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnMenuUnclicked(e);
+            }
+            else
+            {
+                MenuButtonDown = false;
+                MenuButtonUp = false;
+            }
+
+            pad = controllerState.ulButtonTouched & (1UL << ((int)EVRButtonId.k_EButton_SteamVR_Touchpad));
+            if (pad > 0L && !padTouched)
+            {
+                padTouchedDown = true;
+                padTouched = true;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnPadTouched(e);
+            }
+            else if (pad == 0L && padTouched)
+            {
+                padTouchedUp = true;
+                padTouched = false;
+                ClickedEventArgs e;
+                e.controllerIndex = controllerIndex;
+                e.flags = (uint)controllerState.ulButtonPressed;
+                e.padX = controllerState.rAxis0.x;
+                e.padY = controllerState.rAxis0.y;
+                OnPadUntouched(e);
+            }
+            else
+            {
+                padTouchedDown = false;
+                padTouchedUp = false;
+            }
+        }
+    }
+}
---
+++ Samples/DaydreamLabsControllerPlayground/Assets/SteamVR/Scripts/SteamVR_TrackedObject.cs
@@ -0,0 +1,109 @@
+﻿//========= Copyright 2014, Valve Corporation, All rights reserved. ===========
+//
+// Purpose: For controlling in-game objects with tracked devices.
+//
+//=============================================================================
+
+using UnityEngine;
+using Valve.VR;
+
+public class SteamVR_TrackedObject : MonoBehaviour
+{
+	public enum EIndex
+	{
+		None = -1,
+		Hmd = (int)OpenVR.k_unTrackedDeviceIndex_Hmd,
+		Device1,
+		Device2,
+		Device3,
+		Device4,
+		Device5,
+		Device6,
+		Device7,
+		Device8,
+		Device9,
+		Device10,
+		Device11,
+		Device12,
+		Device13,
+		Device14,
+		Device15
+	}
+
+	public EIndex index;
+	public Transform origin; // if not set, relative to parent
+    public bool isValid = false;
+    public Vector3 velocity = Vector3.zero;
+    public Vector3 angularVelocity = Vector3.zero;
+
+    private void OnNewPoses(params object[] args)
+	{
+		if (index == EIndex.None)
+			return;
+
+		var i = (int)index;
+
+        isValid = false;
+		var poses = (Valve.VR.TrackedDevicePose_t[])args[0];
+		if (poses.Length <= i)
+			return;
+
+		if (!poses[i].bDeviceIsConnected)
+			return;
+
+		if (!poses[i].bPoseIsValid)
+			return;
+
+        isValid = true;
+
+		var pose = new SteamVR_Utils.RigidTransform(poses[i].mDeviceToAbsoluteTracking);
+
+		if (origin != null)
+		{
+			pose = new SteamVR_Utils.RigidTransform(origin) * pose;
+			pose.pos.x *= origin.localScale.x;
+			pose.pos.y *= origin.localScale.y;
+			pose.pos.z *= origin.localScale.z;
+			transform.position = pose.pos;
+			transform.rotation = pose.rot;
+        }
+		else
+		{
+			transform.localPosition = pose.pos;
+			transform.localRotation = pose.rot;
+		}
+
+        velocity.x = poses[i].vVelocity.v0;
+        velocity.y = poses[i].vVelocity.v1;
+        velocity.z = poses[i].vVelocity.v2;
+
+        angularVelocity.x = poses[i].vAngularVelocity.v0;
+        angularVelocity.y = poses[i].vAngularVelocity.v1;
+        angularVelocity.z = poses[i].vAngularVelocity.v2;
+    }
+
+	void OnEnable()
+	{
+		var render = SteamVR_Render.instance;
+		if (render == null)
+		{
+			enabled = false;
+			return;
+		}
+
+		SteamVR_Utils.Event.Listen("new_poses", OnNewPoses);
+	}
+
+	void OnDisable()
+	{
+		SteamVR_Utils.Event.Remove("new_poses", OnNewPoses);
+		isValid = false;
+	}
+
+	public void SetDeviceIndex(int index)
+	{
+		if (System.Enum.IsDefined(typeof(EIndex), index))
+			this.index = (EIndex)index;
+	}
+}
+
